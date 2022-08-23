69b909cb0a1506e7f1a3aafb687e4e034643650a
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-23 16:55:23 +0100

    started on header with dropdown

--- react_demo/src/MobileSuit.js
+++ react_demo/src/MobileSuit.js
@@ -2,10 +2,19 @@ import './MobileSuit.css';
 import MobileSuitImg from './MobileSuitImage';
 import NuGundam from './data/NuGundam.json';
 import MobileSuitInfo from './MobileSuitInfo';
+import MobileSuitList from './data/MobileSuits.json'
 
 function MobileSuit(){
     return(
         <>
+            <header class="ms-header">
+                <h1>Select a Mobile Suit</h1>
+                {/* <select name="mobile-suit-list" id="mobile-suit-list">
+                    {MobileSuitList.map(MobileSuitList => (<option></option>))
+
+                    }}
+                </select> */}
+            </header>
             <h1 class="ms-heading">{NuGundam.alias}</h1>
             <div class="ms-body">
                 <MobileSuitInfo nameProp={NuGundam.name} aliasProp={NuGundam.alias} pilotProp={NuGundam.pilot} 
