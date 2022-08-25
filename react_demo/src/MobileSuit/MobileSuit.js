4b43c4a9235976be9af18061db3d865f25d3faf7
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-25 11:13:40 +0100

    use lifting states to make selection component

--- react_demo/src/MobileSuit/MobileSuit.js
+++ react_demo/src/MobileSuit/MobileSuit.js
@@ -2,31 +2,26 @@ import './MobileSuit.css';
 import MobileSuitImg from './MobileSuitImage';
 //import NuGundam from './data/NuGundam.json';
 import MobileSuitInfo from './MobileSuitInfo';
-import MobileSuitList from './data/MobileSuits.json'
+import MobileSuitList from '../data/MobileSuits.json'
+import MobileSuitSelect from './MobileSuitSelect';
 import {useState} from 'react'
 
 const MobileSuit = () => {
     let initialMS = MobileSuitList[0]
+    //console.log(MobileSuitList)
+
     const[currentMS, setMS] = useState({...initialMS});
 
     const handleChange = index => {
         let newMS = MobileSuitList[index];
-        setMS({...newMS})
+        setMS({...newMS});
     }
 
     return(
             <>
-                <header className="ms-header">
-                    <h1>Select a Mobile Suit</h1>
-                    <select name="mobile-suit-list" id="mobile-suit-list" className="ms-select"
-                    onChange={(e) => handleChange(e.target.value)}>
-                        {MobileSuitList.map((ms, i) => (<option key={i} label={ms.alias} value={i}></option>))}
-                    </select>
-                </header>
+                <MobileSuitSelect currentMS={currentMS} handleChange={handleChange} list={MobileSuitList}/>
                 <h1 className="ms-heading">{currentMS.alias}</h1>
                 <div className="ms-body">
-                    {/* <MobileSuitInfo {...initialMS}/>
-                    <MobileSuitImg pathProp={initialMS.imagepath} urlProp={initialMS.imageurl} altProp={initialMS.alias}/> */}
                     <MobileSuitInfo {...currentMS}/>
                     {/* <MobileSuitImg pathProp={currentMS.imagepath} urlProp={currentMS.imageurl} altProp={currentMS.alias}/> */}
                 </div>
--- react_demo/src/MobileSuit/MobileSuitImage.js
+++ react_demo/src/MobileSuit/MobileSuitImage.js
@@ -1,7 +1,8 @@
 function MobileSuitImg(props){
 
     //console.log(props.pathProp);
-    let msimg = require(`${props.pathProp}`);
+    let msimg = require(`../images/${props.pathProp}`);
+    console.log(msimg);
     return(
         <a href={props.urlProp} target="_blank"  rel="noopener noreferrer">
             <img src={msimg} alt = {props.altProp} className="ms-image" height="540" width="auto"/>
