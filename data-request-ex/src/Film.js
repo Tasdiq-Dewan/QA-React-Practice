e09e18832c091152c98b1e8a8f1c18918043f698
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-25 16:54:56 +0100

    usestates for film component

---
+++ data-request-ex/src/Film.js
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+
+function Film(){
+
+    const [filmTitle, setTitle] = useState('');
+    const [date, setDate] = useState("");
+    const [apiKey, setKey] = useState("8d44fde4");
+
+    return(
+        <>
+        </>
+    );
+}
+
+export default Film;
