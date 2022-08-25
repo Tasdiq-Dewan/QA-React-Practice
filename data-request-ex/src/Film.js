a2d0131fe749340e9671a76d90ea78782ee356cf
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-25 17:09:25 +0100

    added input for film title and button, prints repsonse to console

--- data-request-ex/src/Film.js
+++ data-request-ex/src/Film.js
@@ -1,13 +1,29 @@
 import {useState} from 'react'
+import axios from 'axios'
 
 function Film(){
 
     const [filmTitle, setTitle] = useState('');
-    const [date, setDate] = useState("");
+    const [data, setData] = useState("");
     const [apiKey, setKey] = useState("8d44fde4");
+
+    const updateTitle = (e) => {
+        e.preventDefault();
+        setTitle(e.target.value.replace(/\s/g, "%20"));
+    }
+
+    const getFilmDate = (e) => {
+        axios.get(`https://www.omdbapi.com/?apikey=${apiKey}&t=${filmTitle}`)
+        .then(res => {
+            console.log(res.data);
+            setData(res.data);
+        })
+    }
 
     return(
         <>
+            <input type="text" onChange={(e) => updateTitle(e)}/>
+            <button type="button" onClick={(e) => getFilmDate(e)}>Search film</button>
         </>
     );
 }
